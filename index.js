576be8c306294515105e9a25ccc6ac48a0c63db7
Tbinih <jjhhbb0930@naver.com>
2020-06-25 23:30:33 +0900

    bot upload

--- index.js
+++ index.js
@@ -18,53 +18,30 @@ client.on('message', (message) => {
     }
   });
 
-  if(message.content.startsWith('!청소')) {
-    if(checkPermission(message)) return
-
-    var clearLine = message.content.slice('!청소 '.length);
-    var isNum = !isNaN(clearLine)
-
-    if(isNum && (clearLine <= 0 || 100 < clearLine)) {
-      message.channel.send("1부터 100까지의 숫자만 입력해주세요.")
-      return;
-    } else if(!isNum) { // c @티빈이 3
-      if(message.content.split('<@').length == 2) {
-        if(isNaN(message.content.split(' ')[2])) return;
-
-        var user = message.content.split(' ')[1].split('<@!')[1].split('>')[0];
-        var count = parseInt(message.content.split(' ')[2])+1;
-        const _limit = 10;
-        let _cnt = 0;
-
-        message.channel.fetchMessages({limit: _limit}).then(collected => {
-          collected.every(msg => {
-            if(msg.author.id == user) {
-              msg.delete();
-              ++_cnt;
-            }
-            return !(_cnt == count);
-          });
+  client.on('message', (message) => {
+    if(message.content.startsWith('!전체공지')) {
+      if(checkPermission(message)) return
+      if(message.member != null) { // 채널에서 공지 쓸 때
+        let contents = message.content.slice('!전체공지'.length);
+        message.member.guild.members.array().forEach(x => {
+          if(x.user.bot) return;
+          x.user.send(`<@${message.author.id}> ${contents}`);
         });
+    
+        return message.reply('공지를 전송했습니다.');
+      } else {
+        return message.reply('채널에서 실행해주세요.');
       }
+    }
+  });
+
+  function checkPermission(message) {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+      message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
+      return true;
     } else {
-      message.channel.bulkDelete(parseInt(clearLine)+1)
-        .then(() => {
-          AutoMsgDelete(message, `<@${message.author.id}> ` + parseInt(clearLine) + "개의 메시지를 삭제했습니다. (이 메세지는 잠시 후에 사라집니다.)");
-        })
-        .catch(console.error)
+      return false;
     }
   }
-});
-
-
-  message.channel.send(embed)
-} else if(message.content == 'embed2') {
-  let helpImg = 'https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png';
-  let commandList = [
-    {name: 'ping', desc: '현재 핑 상태'},
-    {name: 'embed', desc: 'embed 예제1'},
-    {name: 'embed2', desc: 'embed 예제2 (help)'},
-    {name: '!전체공지', desc: 'dm으로 전체 공지 보내기'},
-  ];
 
 client.login(token);
