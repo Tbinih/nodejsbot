f9a6f26b499bf5c399aa7e2dea45e1465d6c97bd
Tbinih <67421848+Tbinih@users.noreply.github.com>
2020-07-02 13:42:27 +0900

    bot upload

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ client.on('ready', () => {
   client.user.setPresence({ game: { name: '!도움말을 쳐주세요!' }, status: 'online' })
 });
 
-client.on('message', (message) => {
-  if(message.content === '!도움말') {
-    message.reply('```!길드 마스터, !길드 서브 마스터``` (!전체공지[관리자전용])');
-  }
-});
+    message.channel.send(embed)
+  } else if(message.content == '!도움말') {
+    let helpImg = 'https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png';
+    let commandList = [
+      {name: '!길드 마스터', desc: '현재 길드 마스터'},
+      {name: '!길드 서브 마스터', desc: '현제 길드 서브 마스터'},
+      {name: '!전체공지', desc: 'dm으로 전체 공지 보내기[관리자 전용]'},
+    ];
 
 client.on('message', (message) => {
   if(message.content === '!길드 마스터') {
