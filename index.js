e4c16363160cd567f99273b0401060e391a0307a
Tbinih <67421848+Tbinih@users.noreply.github.com>
2020-07-02 14:54:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ client.on('ready', () => {
   client.user.setPresence({ game: { name: '!help를 쳐보세요.' }, status: 'online' })
 });
 
+client.on('message', (message) => {
+  if(message.author.bot) return;
+
+  if(message.content == 'ping') {
+    return message.reply('pong');
+  }
+
   if(message.content == 'embed') {
     let img = 'https://cdn.discordapp.com/icons/419671192857739264/6dccc22df4cb0051b50548627f36c09b.webp?size=256';
     let embed = new Discord.RichEmbed()
